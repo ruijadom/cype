900496eb00b85b82d6d8deca7108b4e021a9bd79
ruijadom <ruijadomingues@gmail.com>
2021-11-29 10:10:33 +0000

    fix(cypress): plugin env congif

--- test/e2e/src/integration/app.spec.ts
+++ test/e2e/src/integration/app.spec.ts
@@ -1,18 +1,18 @@
 describe("app", () => {
   beforeEach(() => {
-    cy.visit("");
-  })
+    cy.visit("/");
+  });
 
   it("should test header 1", () => {
     cy.get("h1").should("be.visible").contains("Name: cype");
-  })
+  });
   it("should test header 2", () => {
     cy.get("h2").should("be.visible").contains("Framework: react");
-  })
+  });
   it("should test header 3", () => {
     cy.get("h3").should("be.visible").contains("Language: TypeScript");
-  })
+  });
   it("should test header 4", () => {
     cy.get("h4").should("be.visible").contains("CSS: Tailwind");
-  })
-})
+  });
+});
